984a3a7ae123d5261862317ec9ebda08a2e8393b
Atmo Suwiryo <suwiryo.atmo@gmail.com>
2025-12-09 20:38:47 +0700

    refactor(ci): test actions

--- datatable-api/src/app/services/prisma.service.ts
+++ datatable-api/src/app/services/prisma.service.ts
@@ -1,12 +1,10 @@
 import { Injectable, OnModuleInit } from '@nestjs/common';
-import { PrismaClient } from '@prisma/client';
+
+import { PrismaClient } from '#prisma/prisma-client';
 
 @Injectable()
-export class PrismaService extends PrismaClient
-  implements OnModuleInit {
-
+export class PrismaService extends PrismaClient implements OnModuleInit {
   async onModuleInit() {
     await this.$connect();
   }
-
 }
