6c5f3553d111a10fad72390cbe1d7c70b3874d18
Atmo Suwiryo <suwiryo.atmo@gmail.com>
2025-12-11 15:17:12 +0700

    refactor(ci): test actions

--- datatable-api/entrypoint.sh
+++ datatable-api/entrypoint.sh
@@ -2,11 +2,11 @@
 
 set -e
 
-# echo "Running database migrations..."
-# npx prisma migrate deploy --schema datatable-api/prisma/schema.prisma
+echo "Running database migrations..."
+npx prisma migrate deploy --schema datatable-api/prisma/schema.prisma
 
-echo "Running database reset & seeder..."
-npm run prisma:reset -- -f
+# echo "Running database reset & seeder..."
+# npm run prisma:reset -- -f
 
 echo "Starting the application..."
 exec "$@"
