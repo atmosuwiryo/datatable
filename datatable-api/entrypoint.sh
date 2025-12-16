83453b3cca7d9381fc6396d332cd789139079c39
Atmo <suwiryo.atmo@gmail.com>
2025-12-17 05:09:00 +0700

    Test dockerhub (#4)
    * refactor(ci): test dockerhub
    
    * refactor(ci): update node & pnpm version
    
    * refactor(ci): test actions
    
    * refactor(ci): test actions
    
    * refactor(ci): test actions
    
    * refactor(ci): test actions
    
    * refactor(ci): test actions
    
    * refactor(ci): test actions
    
    * refactor(ci): test actions
    
    * refactor(ci): test actions
    
    * refactor(ci): test actions
    
    * refactor(ci): test actions
    
    * refactor(ci): test actions
    
    * refactor(ci): test actions
    
    * refactor(ci): test actions
    
    * refactor(ci): test actions
    
    * refactor(ci): test actions
    
    * refactor(ci): test actions
    
    * refactor(ci): test actions
    
    * refactor(ci): test actions
    
    * refactor(ci): test actions
    
    * refactor(ci): test actions
    
    * refactor(ci): test actions
    
    * refactor(ci): test actions
    
    * refactor(ci): test actions
    
    * refactor(ci): test actions
    
    * refactor(ci): test actions
    
    * refactor(ci): test actions
    
    * refactor(ci): test actions
    
    * refactor(ci): test actions
    
    * refactor(ci): test actions
    
    * refactor(ci): test actions
    
    * refactor(ci): test actions
    
    * refactor(ci): test actions
    
    * refactor(ci): test actions
    
    * refactor(ci): test actions
    
    * refactor(ci): test actions
    
    * refactor(ci): test actions
    
    * refactor(ci): test actions
    
    * refactor(ci): test actions
    
    * refactor(ci): test actions
    
    * refactor(ci): test actions
    
    * refactor(ci): test actions
    
    * refactor(ci): test actions
    
    * refactor(ci): test actions
    
    * refactor(ci): test actions
    
    * refactor(ci): test actions

--- datatable-api/entrypoint.sh
+++ datatable-api/entrypoint.sh
@@ -5,5 +5,8 @@ set -e
 echo "Running database migrations..."
 npx prisma migrate deploy --schema datatable-api/prisma/schema.prisma
 
+# echo "Running database reset & seeder..."
+# npm run prisma:reset -- -f
+
 echo "Starting the application..."
 exec "$@"
